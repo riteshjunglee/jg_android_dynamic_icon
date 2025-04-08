8614ca3640a58e8547faeed996df6d8ff1174581
Shivam Piyush <shivam.piyush@jungleegames.com>
2025-04-08 17:02:15 +0530

    Implement screen lock receiver to update app icon on screen off

--- android/src/main/java/com/howzat/jg_android_dynamic_icon/JgAndroidDynamicIconPlugin.java
+++ android/src/main/java/com/howzat/jg_android_dynamic_icon/JgAndroidDynamicIconPlugin.java
@@ -1,66 +1,83 @@
 package com.howzat.jg_android_dynamic_icon;
 
 import android.app.Activity;
-import android.app.Application;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
-import android.os.Bundle;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import android.content.Context;
-import androidx.lifecycle.Lifecycle;
-import androidx.lifecycle.LifecycleEventObserver;
-import androidx.lifecycle.LifecycleOwner;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.embedding.engine.plugins.activity.ActivityAware;
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
-import io.flutter.plugin.common.MethodChannel.Result;
 
-/**
- * JgAndroidDynamicIconPlugin
- */
-public class JgAndroidDynamicIconPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
-    /// The MethodChannel that will the communication between Flutter and native Android
-    ///
-    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-    /// when the Flutter Engine is detached from the Activity
-    private MethodChannel channel;
+import java.util.List;
+
+public class JgAndroidDynamicIconPlugin implements FlutterPlugin, MethodChannel.MethodCallHandler, ActivityAware {
     private static final String TAG = "JgAndroidDynamicIcon";
     private Context context;
     private Activity activity;
-
-    private static List<String> classNames = null;
-    private static boolean iconChanged = false;
-    private static List<String> args = new ArrayList<>();
+    private MethodChannel channel;
+    private List<String> classNames;
+    private List<String> args;
+    private boolean iconChanged = false;
+    private ScreenLockReceiver screenLockReceiver;
 
     @Override
     public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
         channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "jg_android_dynamic_icon");
         channel.setMethodCallHandler(this);
         this.context = flutterPluginBinding.getApplicationContext();
+        registerScreenLockReceiver();
     }
 
     @Override
-    public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-        Log.i(TAG, "onMethodCall");
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        channel.setMethodCallHandler(null);
+        unregisterScreenLockReceiver();
+        this.context = null;
+    }
+
+    @Override
+    public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
+        this.activity = binding.getActivity();
+        this.context = activity.getApplicationContext();
+        registerScreenLockReceiver();
+    }
+
+    @Override
+    public void onDetachedFromActivity() {
+        unregisterScreenLockReceiver();
+        this.activity = null;
+    }
+
+    @Override
+    public void onDetachedFromActivityForConfigChanges() {
+        this.activity = null;
+    }
+
+    @Override
+    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
+        this.activity = binding.getActivity();
+        this.context = activity.getApplicationContext();
+    }
+
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull MethodChannel.Result result) {
         switch (call.method) {
-            case "getPlatformVersion":
-                result.success("Android " + android.os.Build.VERSION.RELEASE);
-                break;
             case "initialize":
                 classNames = call.arguments();
+                result.success(null);
                 break;
             case "changeIcon":
                 changeIcon(call);
+                result.success(null);
                 break;
             default:
                 result.notImplemented();
@@ -68,71 +85,78 @@ public class JgAndroidDynamicIconPlugin implements FlutterPlugin, MethodCallHand
         }
     }
 
-    @Override
-    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-        channel.setMethodCallHandler(null);
-        this.context = null;
-    }
-
-    @Override
-    public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
-        Log.i(TAG, "onAttachedToActivity");
-        this.activity = binding.getActivity();
-
-        Lifecycle lifecycle = ((LifecycleOwner) activity).getLifecycle();
-
-        // Observe the lifecycle
-    }
-
-    @Override
-    public void onDetachedFromActivityForConfigChanges() {
-        Log.i(TAG, "onDetachedFromActivityForConfigChanges");
-        this.activity = null;
-    }
-
-    @Override
-    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
-        Log.i(TAG, "onReattachedToActivityForConfigChanges");
-        this.activity = binding.getActivity();
-    }
-
-    @Override
-    public void onDetachedFromActivity() {
-        Log.i(TAG, "onDetachedFromActivity");
-        this.activity = null;
-    }
-
     private void changeIcon(MethodCall call) {
         if (classNames == null || classNames.isEmpty()) {
-            Log.e(TAG, "Initialization Failed!");
-            Log.i(TAG, "List all the activity-alias class names in initialize()");
+            Log.e(TAG, "Initialization Failed! Provide activity-alias class names.");
+            return;
+        }
+        args = call.arguments();
+        iconChanged = false;
+    }
+
+    private void scheduleIconUpdate() {
+        if (!iconChanged)
+            enqueueIconChange();
+    }
+
+    private void enqueueIconChange() {
+        if (context == null || args == null || args.isEmpty()) {
+            Log.e(TAG, "enqueueIconChange: Missing context or icon arguments.");
             return;
         }
 
-        args = call.arguments();
-        iconChanged = true;
-        updateIcon();
-    }
-
-    void updateIcon() {
-        if (iconChanged) {
-            String className = args.get(0);
-            PackageManager pm = activity.getPackageManager();
-            String packageName = activity.getPackageName();
-            int componentState = PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
-            int i = 0;
-            for (String alias : classNames) {
-                ComponentName cn = new ComponentName(packageName, alias);
-                componentState = className.equals(alias) ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED : PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
-                pm.setComponentEnabledSetting(cn, componentState, PackageManager.DONT_KILL_APP);
-            }
-
-            if (i > classNames.size()) {
-                Log.e(TAG, "class name " + className + " did not match in the initialized list.");
+        String newIconClass = args.get(0);
+        try {
+            PackageManager pm = context.getPackageManager();
+            if (pm == null) {
+                Log.e(TAG, "enqueueIconChange: PackageManager is null!");
                 return;
             }
-            iconChanged = false;
-            Log.d(TAG, "Icon switched to " + className);
+
+            for (String iconClass : classNames) {
+                pm.setComponentEnabledSetting(
+                        new ComponentName(context, iconClass),
+                        PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                        PackageManager.DONT_KILL_APP);
+            }
+
+            pm.setComponentEnabledSetting(
+                    new ComponentName(context, newIconClass),
+                    PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                    PackageManager.DONT_KILL_APP);
+
+            Log.i(TAG, "enqueueIconChange: Icon changed to " + newIconClass);
+            iconChanged = true;
+
+        } catch (Exception e) {
+            Log.e(TAG, "enqueueIconChange: Failed to change icon.", e);
+        }
+    }
+
+    private void registerScreenLockReceiver() {
+        if (context == null || screenLockReceiver != null)
+            return;
+
+        screenLockReceiver = new ScreenLockReceiver();
+        context.registerReceiver(screenLockReceiver, new IntentFilter(Intent.ACTION_SCREEN_OFF));
+        Log.i(TAG, "ScreenLockReceiver registered.");
+    }
+
+    private void unregisterScreenLockReceiver() {
+        if (context != null && screenLockReceiver != null) {
+            context.unregisterReceiver(screenLockReceiver);
+            screenLockReceiver = null;
+            Log.i(TAG, "ScreenLockReceiver unregistered.");
+        }
+    }
+
+    private class ScreenLockReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (Intent.ACTION_SCREEN_OFF.equals(intent.getAction())) {
+                Log.i(TAG, "Screen Locked! Updating app icon...");
+                scheduleIconUpdate();
+            }
         }
     }
 }
